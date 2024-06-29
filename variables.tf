6714a7e577bdb18ba4cd9656c9a8b38fca2a085e
TeJ4s <141610398+Tejas4322@users.noreply.github.com>
2024-06-29 20:18:24 +0530

    Added variable file

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "aws_region" {
+       description = "The AWS region to create things in." 
+       default     = "us-east-1" # Replace with your refion name
+}
+
+variable "lambda_role" { 
+    description = "Name of security group" 
+    default     = "s3_to_rds_lambda_role"
+}
+
+variable "execution_policy" { 
+    description = "Name of security group" 
+    default     = "lambda_basic_execution_policy"
+}
+
+variable "s3_policy" { 
+    description = "Name of security group" 
+    default     = "lambda_s3_full_access_policy"
+}
+
+variable "rds_policy" { 
+    description = "Name of security group" 
+    default     = "lambda_rds_full_access_policy"
+}
+
+variable "cloudwatch_policy" { 
+    description = "Name of security group" 
+    default     = "lambda_cloudwatch_full_access_policy"
+}
+
+variable "my_ecr_repo" {
+    description = "Name of ECR repository"
+    default = "s3-to-rds" # Replace with your ECR repository name
+}
+
+variable "lambda_name" {
+    description = "Name of lambda function"
+    default = "s3-to-rds" # Replace with your ECR repository name
+}
